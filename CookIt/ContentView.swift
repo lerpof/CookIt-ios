4f9d972371455aee74605cafea0350216271cb35
Leonardo Lazzari <le.lazzari@reply.it>
2022-07-26 20:29:08 +0200

    Initial Commit

---
+++ CookIt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CookIt
+//
+//  Created by Leonardo Lazzari on 26/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CookIt/CookItApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookItApp.swift
+//  CookIt
+//
+//  Created by Leonardo Lazzari on 26/07/22.
+//
+
+import SwiftUI
+
+@main
+struct CookItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
